5c63d82b41b46420307c0a48de4b9ff1f8a4c677
Owen McFadden <omcfadden@ucsd.edu>
2026-04-15 14:56:53 -0700

    step 4 test

--- global.js
+++ global.js
@@ -39,3 +39,31 @@ for (let p of pages) {
     a.target = '_blank';
   }
 }
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `<label class="color-scheme">
+    Theme:
+    <select>
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>`,
+);
+
+function setColorScheme(colorScheme) {
+  document.documentElement.style.setProperty('color-scheme', colorScheme);
+  select.value = colorScheme;
+}
+
+const select = document.querySelector('.color-scheme select');
+
+if ('colorScheme' in localStorage) {
+  setColorScheme(localStorage.colorScheme);
+}
+
+select.addEventListener('input', function (event) {
+  localStorage.colorScheme = event.target.value;
+  setColorScheme(event.target.value);
+});
