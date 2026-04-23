92a363fb28899742dae78f1ddc53ee063df3f12f
Owen McFadden <omcfadden@ucsd.edu>
2026-04-23 10:53:40 -0700

    test step 3 again

--- index.js
+++ index.js
@@ -7,4 +7,15 @@ const projectsContainer = document.querySelector('.projects');
 renderProjects(latestProjects, projectsContainer, 'h2');
 
 const githubData = await fetchGitHubData('owenmcfadden2');
-console.log(githubData);
+
+const profileStats = document.querySelector('#profile-stats');
+if (profileStats) {
+  profileStats.innerHTML = `
+    <dl>
+      <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+      <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+      <dt>Followers:</dt><dd>${githubData.followers}</dd>
+      <dt>Following:</dt><dd>${githubData.following}</dd>
+    </dl>
+  `;
+}
